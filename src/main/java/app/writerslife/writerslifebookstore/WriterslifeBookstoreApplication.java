4b47ec85230c42d12293663d56f9cc4326feebec
guhaAG <guha.agt@gmail.com>
2019-10-26 21:21:17 +0900

    init document store api

---
+++ src/main/java/app/writerslife/writerslifebookstore/WriterslifeBookstoreApplication.java
@@ -0,0 +1,13 @@
+package app.writerslife.writerslifebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriterslifeBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriterslifeBookstoreApplication.class, args);
+	}
+
+}
